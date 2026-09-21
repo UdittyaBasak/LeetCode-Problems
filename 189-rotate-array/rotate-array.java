980b163919ced5c03d91515e7793f7bc4dafa2fe
Udittya Basak <udittyabasak02@gmail.com>
2026-09-21 23:55:09 +0530

    Time: 4 ms (60.77%) | Memory: 269 MB (10.94%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,20 +1,46 @@
 class Solution {
     public void rotate(int[] nums, int k) {
 
-        k = k % nums.length;
+        // int temp[] = new int[k+1];
 
-        int temp[] = new int[k];
+        // for (int i = 0; i <= k; i++) {
+        //     temp[i] = nums[i];
+            
+        // }
+                
 
-        for (int i = 0; i < k; i++) {
-            temp[i] = nums[nums.length - k + i];
-        }
 
-        for (int i = nums.length - 1; i >= k; i--) {
-            nums[i] = nums[i - k];
-        }
+        // for (int i = k + 1; i < nums.length; i++) {
 
-        for (int i = 0; i < k; i++) {
-            nums[i] = temp[i];
+        //     nums[i-(k+1)] = nums[i];
+
+        // }
+
+        // int j = 0;
+        // // for (int i = k+1; i < nums.length ; i++) {
+        // //     nums[nums.length-k] = temp[i - (k+1)];
+        // // }
+            
+        // for (int i = k; i < nums.length; i++ ){
+        //     nums[i] = temp[j];
+        //     j++;
+        // }   
+
+        k = k % nums.length; //basically you are make the k smaller 
+
+        reverse(nums, 0, nums.length - k - 1);
+        reverse(nums, nums.length - k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+    }
+
+    public void reverse(int arr[], int start, int end){
+        while(start < end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+
+            start ++;
+            end --;
         }
     }
 }
